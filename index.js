5d80630f6d5ffc35131ec243e90acadb913bcc10
Kevin Martensson <kevinmartensson@gmail.com>
2015-03-15 13:03:43 +0100

    Tweaks

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ var got = require('got');
 var page = 1;
 var ret = [];
 
-function getRepos(user, opts, cb) {
+function getRepos(user, headers, cb) {
 	var url = 'https://api.github.com/users/' + user + '/repos?&per_page=100&page=' + page;
-	var auth = [opts.username, opts.password].join(':');
-	var headers = {
-		Accept: 'application/vnd.github.v3+json',
-		Authorization: 'Basic ' + new Buffer(auth).toString('base64')
-	};
 
 	got(url, {headers: headers}, function (err, data, res) {
 		if (err) {
@@ -18,11 +13,11 @@ function getRepos(user, opts, cb) {
 			return;
 		}
 
-		ret.push(JSON.parse(data));
+		ret = ret.concat(JSON.parse(data));
 
 		if (res.headers.link.indexOf('next') !== -1) {
 			page++;
-			getRepos(user, opts, cb);
+			getRepos(user, headers, cb);
 			return;
 		}
 
@@ -37,7 +32,13 @@ module.exports = function (user, opts, cb) {
 		throw new Error('Username and password is required');
 	}
 
-	getRepos(user, opts, function (err, data) {
+	var auth = [opts.username, opts.password].join(':');
+	var headers = {
+		Accept: 'application/vnd.github.v3+json',
+		Authorization: 'Basic ' + new Buffer(auth).toString('base64')
+	};
+
+	getRepos(user, headers, function (err, data) {
 		if (err) {
 			cb(err);
 			return;
