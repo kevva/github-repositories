e01043cd55469f26189d25737fe08713b789759d
Kevin Martensson <kevinmartensson@gmail.com>
2015-03-15 13:47:08 +0100

    Add test using a public token

--- test.js
+++ test.js
@@ -4,6 +4,11 @@ var githubRepos = require('./');
 var test = require('ava');
 
 test(function (t) {
-	t.assert(true);
-	t.end();
+	t.plan(2);
+	var token = '523ef691191741c99d5afbcfe58079bfa0038771';
+
+	githubRepos('kevva', {token: token}, function (err, data) {
+		t.assert(!err, err);
+		t.assert(data.length);
+	});
 });
