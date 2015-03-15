69a0f954e15e976e572aa7468c217230f620e42d
tunnckoCore <mameto_100@mail.bg>
2015-03-15 17:46:42 +0200

    add one more test

--- test.js
+++ test.js
@@ -3,11 +3,21 @@
 var githubRepos = require('./');
 var test = require('ava');
 
-test(function (t) {
+test('user with more than 100 repos', function (t) {
 	t.plan(2);
 	var token = '523ef691191741c99d5afbcfe58079bfa0038771';
 
 	githubRepos('kevva', {token: token}, function (err, data) {
+		t.assert(!err, err);
+		t.assert(data.length);
+	});
+});
+
+test('user with lower than 100 repos', function (t) {
+	t.plan(2);
+	var token = '523ef691191741c99d5afbcfe58079bfa0038771';
+
+	githubRepos('AniSoft', {token: token}, function (err, data) {
 		t.assert(!err, err);
 		t.assert(data.length);
 	});
