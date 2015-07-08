41c4f7f557aa24983a6367a715bdfcc3f4e7ff78
Parsha Pourkhomami <parshap+git@gmail.com>
2015-07-08 01:35:24 -0700

    Test two subsequent requests get the same data

--- test.js
+++ test.js
@@ -22,3 +22,18 @@ test('user with lower than 100 repos', function (t) {
 		t.assert(data.length, data.length);
 	});
 });
+
+test('two requests should return same data', function (t) {
+	t.plan(5);
+	var token = '523ef691191741c99d5afbcfe58079bfa0038771';
+
+	githubRepos('octocat', {token: token}, function (err, data1) {
+		t.assert(!err, err);
+		t.assert(data1.length, data1.length);
+		githubRepos('octocat', {token: token}, function (err, data2) {
+		t.assert(!err, err);
+			t.assert(data2.length, data2.length);
+			t.assert(data1.length === data2.length);
+		});
+	});
+});
