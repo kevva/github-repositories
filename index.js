3edd0c2ca8827429786dccbf9b9e5e78782f5e3f
Richie Bendall <richiebendall@gmail.com>
2022-04-01 21:40:31 +1300

    Fix fetching private repositories for organisation
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 const {Octokit} = require('@octokit/rest');
 
-const octokit = new Octokit();
-
 module.exports = async (name, {
 	sort = 'full_name',
 	direction,
@@ -13,13 +11,29 @@ module.exports = async (name, {
 		throw new TypeError(`Expected \`name\` to be of type \`string\` but received type \`${typeof name}\``);
 	}
 
-	return octokit.paginate(octokit.repos.listForUser, {
-		username: name,
-		per_page: 100, // eslint-disable-line camelcase
-		sort,
-		direction,
+	const octokit = new Octokit({
 		auth,
 		baseUrl,
 		userAgent: 'https://github.com/kevva/github-repositories'
 	});
+
+	const {Type: type} = await octokit.users.getByUsername({
+		username: name
+	});
+
+	const requestOptions = {
+		per_page: 100, // eslint-disable-line camelcase
+		sort,
+		direction
+	};
+
+	return type === 'User' ?
+		octokit.paginate(octokit.repos.listForUser, {
+			username: name,
+			...requestOptions
+		}) :
+		octokit.paginate(octokit.repos.listForOrg, {
+			org: name,
+			...requestOptions
+		});
 };
